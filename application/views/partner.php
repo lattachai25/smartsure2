abd6b0dc59114222ada713f6a41b583f2cf11b59
nextcis <42366333+lattachai25@users.noreply.github.com>
2020-11-10 16:09:48 +0700

    ..

--- application/views/partner.php
+++ application/views/partner.php
@@ -26,9 +26,9 @@ $partners = $query->result();
             <div class="row " >
               <?php foreach($partners as $partner): ?> 
                   <div class="col-3 " >                        
-                    <a href="<?php echo $partner->partner_link;?>" target="_blank">
+                    <!-- <a href="<?php echo $partner->partner_link;?>" target="_blank"> -->
                     <img class="sizePartner" src="<?php echo base_url();?>/assets/uploads/partner/<?php echo $partner->partner_image;?>" />
-                </a>  
+                <!-- </a>   -->
               </div>
                 <br>
               <?php endforeach; ?>  
