6f280adcfac8ee9fad940cd89f81db6c39300693
Napats77 <looksornnilas@gmail.com>
2021-01-04 14:14:00 +0700

    bot_detected

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,17 +9,19 @@ class Home extends CI_Controller {
 		}
 
 	public function index(){
-		$contents['cart_session'] = $this->session->userdata('cart_session');
+	// 	echo $_SERVER['HTTP_USER_AGENT'];
+	// 	echo $this->_bot_detected(); 
+	// exit;	
+	$contents['cart_session'] = $this->session->userdata('cart_session');
 		$template = array(
 			   'title' => 'Home | Smart Sure',
 			   'content' => 'Home',
 			   'description' => 'Smartsure พร้อมดูแลรถของคุณตลอด 24 ชม. ให้คุณอุ่นใจตลอดการเดินทาง',
-			   
           );
 
 		// $template['content'] = $this->load->view('home',$contents,TRUE);
 		$this->load->view('template',$template);
-	
+
 	}
 
 	public function view($id=null){
@@ -35,4 +37,12 @@ class Home extends CI_Controller {
 		$this->load->view('template',$template);
 		}
 	
+
+		function _bot_detected() {
+
+			return (
+			isset($_SERVER['HTTP_USER_AGENT'])
+			&& preg_match('/bot|crawl|slurp|spider|mediapartners/i', $_SERVER['HTTP_USER_AGENT'])
+			);
+			}
 }
