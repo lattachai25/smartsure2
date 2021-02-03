c7d545826a7a95d400ae3e59794bedf17321210a
Napats77 <looksornnilas@gmail.com>
2021-02-03 13:33:53 +0700

    whitecard

--- application/views/whitecardQR.php
+++ application/views/whitecardQR.php
@@ -11,10 +11,34 @@ $WhitecardQR = $query->result();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <img src="<?php base_url(); ?>assets/uploads/whitecard_qr/whitecard.png" width="100%"   alt="" />   
-        <div style="width:50px; height:50px; border:1px solid black;">
-                <a href="www.facebook.com">click here</a>
+
+        <div style="position:relative;">
+            <img  src="<?php base_url(); ?>assets/uploads/whitecard_qr/whitecard.jpg" width="100%"   alt="" />
+            
+            <div class="row">
+                <div class="col-10"></div>
+                <div class="col-2">
+                    <a href="https://line.me/R/ti/p/@wsmart" >
+                        <img class="lineads" src="<?php base_url(); ?>assets/uploads/whitecard_qr/line_button.jpg" width="100%"   alt="" />   
+                    </a>   
+                </div>
+            </div>        
         </div>
     </body>
+
+    <style>
+           
+            .lineads{
+                width: 274px;
+                height: 100px;
+                right: 10%;
+                bottom: 5%;
+                /* top: 0; */
+                /* z-index: 0; */
+                position: absolute;
+
+       
+        }
+    </style>
 </html>
 
